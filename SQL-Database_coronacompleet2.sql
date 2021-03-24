9cde298049f54dabf194692dd8a63049852a9eaa
Joeri332 <74733577+Joeri332@users.noreply.github.com>
2021-03-24 18:16:08 +0100

    Update SQL-Database_coronacompleet2.sql

--- SQL-Database_coronacompleet2.sql
+++ SQL-Database_coronacompleet2.sql
@@ -125,13 +125,13 @@ INSERT INTO `werknemers`(`personeelsnummer`, `naam`, `adres`, `postcode`, `woonp
 --
 
 INSERT INTO `producten` (`productnummer`, `naam`, `prijs`, `image`, `voorraad`) VALUES
-('1', 'Mondkap zwart', '39.95', 'image/zwart1.png', 20 ),
-('2', 'Mondkap blauw', '39.95', 'image/blauw1.png', 20),
-('3', 'Mondkap roze', '39.95', 'image/roze1.png', 20),
-('4', 'Mondkap groen', '39.95', 'image/groen1.png', 20),
-('5', 'Handschoen', '25.95', 'image/handschoen1.jpeg', 20),
-('6', 'Desinfectie', '159.95', 'image/desinfectie1.jpg', 20),
-('7', 'Sneltest', '59.95', 'image/testPic1b.jpeg', 20);
+('1', 'Mondkap zwart', '39.95', 'image/zwart1.png', 100 ),
+('2', 'Mondkap blauw', '39.95', 'image/blauw1.png', 100),
+('3', 'Mondkap roze', '39.95', 'image/roze1.png', 100),
+('4', 'Mondkap groen', '39.95', 'image/groen1.png', 100),
+('5', 'Handschoen', '25.95', 'image/handschoen1.jpeg', 100),
+('6', 'Desinfectie', '159.95', 'image/desinfectie1.jpg', 100),
+('7', 'Sneltest', '59.95', 'image/testPic1b.jpeg', 100);
 
 INSERT INTO `klanten`(`email`, `naam`, `adres`, `postcode`, `woonplaats`,`gebruikersnaam`, `telefoonnummer`, `wachtwoord`) VALUES 
 ('piet@hotmail.com', 'Piet van kelp','marktstraat 16', '5373ae', 'scheveningen', 'piet1', '0638329083', 'wachtwoord1' ),
@@ -156,23 +156,19 @@ INSERT INTO `winkelmand` (`email`, `productnummer`, `aantal`) VALUES
 ('piet@hotmail.com', '2', '1');
 
 
---- sql queries ---
+--- sql querie product selectie en update ---
 
 INSERT INTO winkelmand (email, productnummer, aantal)
 VALUES ('piet@hotmail.com', 3, 3)
 ON DUPLICATE KEY UPDATE aantal = aantal + 3
 
--- bestelgegevens + winkelmand gegevens --
+-- view bestellingen klant -- 
 
-SELECT klanten.email, klanten.naam, klanten.adres, klanten.postcode, klanten.woonplaats, klanten.telefoonnummer, winkelmand.productnummer, winkelmand.aantal
-FROM klanten
-INNER JOIN winkelmand ON winkelmand.email = klanten.email
+CREATE VIEW bestel_overzicht AS
+SELECT ordernummer, betaalmethode, totaalbedrag
+FROM bestellingen
+Where email = piet@hotmail.com -- user login -- 
 
--- klant drukt op bestellen, gegegevens winkelmand worden geladen --
-
-INSERT INTO bestellingen(email)
-SELECT DISTINCT email
-FROM winkelmand
 
 -- start transaction, klant drukt op knop bestellen er wordt hier automatisch ordernummer aangemaakt en geplaatst in orders, bestellingen -- 
 
@@ -188,6 +184,12 @@ INSERT INTO bestellingen values (@ordernummer, @email, 'paypal', @totaalbedrag);
 
 INSERT INTO orders (select @ordernummer, productnummer, aantal from winkelmand);
 
+UPDATE producten p
+INNER JOIN winkelmand w ON p.productnummer = w.productnummer
+SET p.voorraad = p.voorraad - w.aantal;
+
+DELETE FROM winkelmand;
+
 COMMIT;
 
 -- stored procedure voorraad aanpassen-- 
@@ -196,6 +198,29 @@ CREATE PROCEDURE `Voorraad_aanpassen`(IN productnr INT, IN aantal INT)
 INSERT INTO producten(`productnummer`,`voorraad`)
 VALUES (productnr, aantal)
 ON DUPLICATE KEY UPDATE voorraad  =  voorraad + aantal;
+
+-- stored procedure klant bestellingen opzoeken --
+
+CREATE PROCEDURE `klantorders`(IN email_klant varchar(255))
+SELECT ordernummer, betaalmethode, totaalbedrag FROM bestellingen
+where  ordernummer = email_klant
+
+-- stored procedure ordernummer opzoeken --
+
+CREATE PROCEDURE `orderinzicht` (IN orderNR INT)
+SELECT o.productnummer, p.naam, o.aantal FROM orders o 
+INNER JOIN producten p  on o.productnummer = p.productnummer
+WHERE ordernummer = orderNR
+
+-- view van de bestelde producten -- 
+
+SELECT orders.productnummer, producten.naam , SUM(orders.aantal)
+from orders 
+INNER JOIN producten ON producten.productnummer = orders.productnummer
+group by productnummer 
+
+
+-- trigger voorraad laag --
 
 DELIMITER // 
 CREATE TRIGGER voorraad_laag
@@ -210,3 +235,4 @@ BEGIN
    END IF;
 END //
 DELIMITER ; 
+
