0245ee3eb649d2fb1c950bee6f72d54662f20c09
Joeri332 <74733577+Joeri332@users.noreply.github.com>
2021-05-11 16:48:14 +0200

    Update zoekgeschiedenis.php

--- BLOK4/pagina/zoekgeschiedenis.php
+++ BLOK4/pagina/zoekgeschiedenis.php
@@ -6,67 +6,39 @@
   require_once 'header.php';
 
 
-$result = FetchQuery($conn, "SELECT zoekterm, datum, gebruiker, zoekID FROM zoekgeschiedenis");
-$array = array();
 
 
-foreach ($result as $row) {
-
-		echo "<tr>
-		<td>" .$row['zoekterm'] . "</td>
-		<td>" .$row['datum'] . "</td>
-		<td>" .$row['gebruiker'] . "</td>
-        <td>" .$row['zoekID'] . "</td>";
-
-
-	};
+$result = FetchQuery($conn, "SELECT zoekterm, datum, gebruiker, zoekID  FROM zoekgeschiedenis");
 ?>
+
 <div class="container">
   <div class="row">
     <div class="col-12">
       <table class="table table-bordered">
         <thead>
           <tr>
-            <th scope="col">Select Day</th>
-            <th scope="col">Article Name</th>
-            <th scope="col">Author</th>
-            <th scope="col">Words</th>
-            <th scope="col">Shares</th>
+            <th scope="col">Zoekterm</th>
+            <th scope="col">Datum Name</th>
+            <th scope="col">Gebruiker</th>
+            <th scope="col">ZoekID</th>
+
           </tr>
         </thead>
         <tbody>
           <tr>
-            <td>
-              <div class="custom-control custom-checkbox">
-                  <input type="checkbox" class="custom-control-input" id="customCheck1" checked>
-                  <label class="custom-control-label" for="customCheck1">1</label>
-              </div>
-            </td>
-            <?php foreach ($result as $row) {
-            echo"";
-            echo" <td>$row['zoekterm']</td>";
-            echo"<td>.$row['zoekterm']</td>";
+            <?php
+            foreach ($result as $row) {
+            echo "<tr>
+            <td>" . $row['zoekterm'] . "</td>
+            <td>" . $row['datum'] . "</td>
+            <td>" . $row['gebruiker'] . "</td>
+            <td>" . $row['zoekID'] . "</td>
+            <td><input type='checkbox' placeholder = '".$row['zoekID']."'></td>" ;}?>
+            
 
-
-          echo "<a href='".$row["naam"]."Product.php"."'<div class = 'zoekResultaat'>
-          ".$row["naam"]."
-          ".$row["prijs"]." 
-   
-
+          </tr>       
         </tbody>
       </table>
     </div>
   </div>
 </div>
-"
-
-
-
-
-	<footer class="borderfooter">
-		<p><strong>CORONA COMPLEET</strong> in partnership with <a href="https://www.u-earth.eu/">U-EARTH</a></p>
-	</footer>	
-</div>
-
-</body>
-</html>
