31036f193575aaa071b2a4ee8669def661b5b15d
EvelienGeerts <74408449+EvelienGeerts@users.noreply.github.com>
2021-06-08 15:23:36 +0200

    sync voor michiel

--- BLOK4/unittest.php
+++ BLOK4/unittest.php
@@ -1,20 +1,49 @@
 <?php
 use PHPUnit\Framework\TestCase;
 include 'models/functions.php';
+include 'models/config.php';
 
 class unittest extends TestCase {
    
 
-//class unittest extends TestCase {
-    public function testExecuteQuery() {
-    $query = 'SELECT naam FROM producten
-    
 
+    //public function testExecuteQuery() {
+    //$query = 'SELECT naam FROM producten WHERE productnummer = 1';
+    //$stmt = 'Mondkap zwart';
+    //$this->assertEquals('Mondkap zwart', $stmt, 0);
+//}
+/*
+public function testExecuteQuery($conn, $query, $params = array() ) {
+    include 'models/config.php';
+    $query = 'SELECT naam FROM producten WHERE productnummer = 1';
+    $stmt = $conn->prepare($query);
+    $stmt->execute($params);
+    return $stmt;
+    $this->assertEquals('Mondkap zwart', $stmt, 0);
 }
 
+}
+// ExecuteQuery 
 function ExecuteQuery($conn, $query, $params = array())
 {
     $stmt = $conn->prepare($query);
     $stmt->execute($params);
     return $stmt;
+}
+*/
+public function CreateTempUser(){
+    
+} 
+
+}
+
+// Aanmaken van een tijdelijke gebruiker mbv het session_id
+function CreateTempUser($conn)
+{
+    $sessionId = session_id();
+
+    $stmt = $conn->prepare("INSERT INTO klanten (email) VALUES (?);");
+    $stmt->execute([$sessionId]);
+
+    $_SESSION["email"] = $sessionId;
 }
